5d63d5c495c2ac5df9931be87a12c935b65590e5
Vinh Veer <vinhveer123@gmail.com>
2024-07-29 21:46:46 +0700

    first commit

---
+++ practice-quiz.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Learn Vocabulary</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <div class="me-auto mb-2">
+                <a class="navbar-brand" href="/">
+                    <img src="img/image.png" style="width: 100px">
+                </a>
+            </div>
+            <form class="d-flex" role="search" style="width: 100%">
+                <input class="form-control me-2" type="search" placeholder="Enter vocabulary" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+    <h3 class="container mt-5 mb-5 display-6">
+        Practice Quiz
+    </h3>
+    <div class="container">
+        <div class="row">
+            <a class="col-md-3" href="random-vocab.php">
+                <div class="card mb-3">
+                    <img src="img\6102035.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Unit 1 - </h5>
+                        <p class="card-text">Expand your vocabulary with randomly generated words, including
+                            definitions,
+                            examples, and pronunciation.</p>
+                    </div>
+                </div>
+            </a>
+            <a class="col-md-3" href="practice-quiz.php">
+                <div class="card mb-3">
+                    <img src="img\7494959.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Unit 2 - </h5>
+                        <p class="card-text">Test your knowledge with various question types and get instant feedback to
+                            improve
+                            your understanding.</p>
+                    </div>
+                </div>
+            </a>
+            <a class="col-md-3" href="exam-quiz.php">
+                <div class="card">
+                    <img src="img\7498819.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Unit 3 -</h5>
+                        <p class="card-text">Simulate exam conditions with timed quizzes and detailed performance
+                            analysis
+                            to
+                            guide your study plan.</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ random-vocab.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Learn Vocabulary</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <div class="me-auto mb-2">
+                <a class="navbar-brand" href="/">
+                    <img src="img/image.png" style="width: 100px">
+                </a>
+            </div>
+            <form class="d-flex" role="search" style="width: 100%">
+                <input class="form-control me-2" type="search" placeholder="Enter vocabulary" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+    <h3 class="container mt-5 mb-5 display-6">
+        Random Vocab
+    </h3>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            degree
+                        </h4>
+                        <p><span>[digrí]</span> noun</p>
+                        <p>Trình độ, bằng cấp</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                           Example 
+                        </h5>
+                        <p>A bachrlor's <b>degree</b> in engineering is a requirement for this position</p>
+                        <p>Bằng cử nhân về kỹ thuật là một điều cần thiết cho vị trí này</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container mt-5">
+        <button class="btn btn-primary me-3">Ok, next word</button>
+        <button class="btn btn-danger">Report problems</button>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
