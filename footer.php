235135ce76bc0dd91d0d70cf3de3b9ee11eb81b6
Vinh Veer <vinhveer123@gmail.com>
2024-08-01 15:35:50 +0700

    commit

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div class="container-fluid mt-5 mb-5 bs-light-bg-subtle">
     <div class="container">
-        2024 &copy; Learn Vocabulary
+        2024 &copy; Made with <3 by dodoVOCAB
     </div>
 </div>
